4c6b92231ffd8e59be1423aca4d8f28c42fa3168
Shane Smith <shane.smith.baz@gmail.com>
2011-09-18 02:01:37 -0400

    Nuked! Renamed and moved to SiCC at http://github.com/shanesmith/sicc.

--- examples/calc/calc.g
+++
@@ -1,6 +0,0 @@
-Expression -> Sum
-Sum -> Term ((plus|minus) Term)*  [>1]
-Term -> Exp ((multiply|divide) Exp)* [>1]  
-Exp -> Element (exp Element)*  [>1] 
-Element -> number | lparen Sum rparen
-
--- src/TokenizerException.java
+++
@@ -1,9 +0,0 @@
-public class TokenizerException extends Exception {
-  private static final long serialVersionUID = 1L;
-  private int lineNumber = -1;
-  public TokenizerException (Throwable cause) { super(cause); }
-  public TokenizerException (String msg) { super(msg); }
-  public TokenizerException (String msg, int lineNumber) { super(msg); this.lineNumber = lineNumber; }
-  public int getLineNumber() { return lineNumber; }
-  public String toString() { return (lineNumber != -1 ? "[line " + lineNumber + "]" : "") + getMessage(); }
-} // end TokenizerException
